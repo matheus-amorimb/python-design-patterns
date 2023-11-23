7710d19c2493a70de4566f12bb452eddbc72c318
Matheus Batista <mbatista.sarti@gmail.com>
2023-11-22 23:35:31 -0300

    first commit

---
+++ adapter/before/main.py
@@ -0,0 +1,17 @@
+import json
+from experiment import Experiment
+
+"""
+Experiment is the client
+Main is the client interface
+"""
+
+def main() -> None:
+    with open('config.json', encoding='utf-8') as file:
+        config = json.load(file)
+    
+    experiment = Experiment(config)
+    experiment.run()
+
+if __name__ == '__main__':
+    main()
